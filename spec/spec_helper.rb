a1174ce4d2e222127ce397aa7eed2bd5d1dab281
Gordon L. Hempton <ghempton@gmail.com>
2013-02-26 17:04:05 -0800

    fix travis config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require(:default, :test)
 
+ENV['RAILS_ENV'] = 'test'
+
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')).freeze
 $LOAD_PATH << File.join(PROJECT_ROOT, 'lib')
 
@@ -10,6 +12,10 @@ require File.expand_path("../support/app/config/environment.rb",  __FILE__)
 require 'rspec/rails'
 
 require 'support/serialization_helpers'
+
+puts File.expand_path("../support/app/db/migrate/", __FILE__)
+
+ActiveRecord::Migrator.migrate(File.expand_path("../support/app/db/migrate/", __FILE__))
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
